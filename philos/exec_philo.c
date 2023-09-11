842225d316ef63f9c7433030010e02efa6547a62
eweiberl <eweiberl@student.42heilbronn.de>
2023-09-11 18:07:31 +0200

    reorganized functions

--- philos/exec_philo.c
+++ philos/exec_philo.c
@@ -6,11 +6,13 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 16:54:17 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/09/11 16:29:27 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/09/11 18:05:40 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
+
+static void	do_first(t_philosopher *philo);
 
 static void	*one_philo(void *param)
 {
@@ -18,14 +20,48 @@ static void	*one_philo(void *param)
 
 	philo = (t_philosopher *)param;
 	philo->start_time = get_start_time();
+	do_first(philo);
+	while (philo->isdead == false && (philo->num_eaten
+			< philo->philo_var.num_has_to_eat
+			|| philo->philo_var.num_has_to_eat == -2))
+	{
+		if (philo->fork == FREE && philo->fork_left->fork == FREE)
+		{
+			philo->fork = LOCK;
+			philo->fork_left->fork = LOCK;
+			pthread_mutex_lock(&philo->fork_lock);
+			printf("%ld %d has taken a fork\n",
+				get_ms(philo->start_time), philo->id);
+			pthread_mutex_lock(&philo->fork_left->fork_lock);
+			printf("%ld %d has taken a fork\n",
+				get_ms(philo->start_time), philo->id);
+			printf("%ld %d is eating\n", get_ms(philo->start_time), philo->id);
+			wait_ms(philo->philo_var.time_to_eat);
+			pthread_mutex_unlock(&philo->fork_lock);
+			pthread_mutex_unlock(&philo->fork_left->fork_lock);
+			philo->fork = FREE;
+			philo->fork_left->fork = FREE;
+			printf("%ld %d is sleeping\n", get_ms(philo->start_time), philo->id);
+			wait_ms(philo->philo_var.time_to_sleep);
+			printf("%ld %d is thinking\n", get_ms(philo->start_time), philo->id);
+		}
+	}
+	printf("ID: %d Current time since start: %ldms\n", philo->id, get_ms(philo->start_time));
+	return (0);
+}
+
+static void	do_first(t_philosopher *philo)
+{
 	if (philo->fork == FREE && philo->fork_left->fork == FREE)
 	{
 		philo->fork = LOCK;
 		philo->fork_left->fork = LOCK;
 		pthread_mutex_lock(&philo->fork_lock);
-		printf("%ld %d has taken a fork\n", get_ms(philo->start_time), philo->id);
+		printf("%ld %d has taken a fork\n",
+			get_ms(philo->start_time), philo->id);
 		pthread_mutex_lock(&philo->fork_left->fork_lock);
-		printf("%ld %d has taken a fork\n", get_ms(philo->start_time), philo->id);
+		printf("%ld %d has taken a fork\n",
+			get_ms(philo->start_time), philo->id);
 		printf("%ld %d is eating\n", get_ms(philo->start_time), philo->id);
 		wait_ms(philo->philo_var.time_to_eat);
 		pthread_mutex_unlock(&philo->fork_lock);
@@ -41,9 +77,6 @@ static void	*one_philo(void *param)
 		wait_ms(philo->philo_var.time_to_sleep);
 	}
 	printf("%ld %d is thinking\n", get_ms(philo->start_time), philo->id);
-	printf("ID: %d Current time since start: %ldms\n", philo->id, get_ms(philo->start_time));
-
-	return (0);
 }
 
 int	create_threads(t_philosopher *philo_list)
