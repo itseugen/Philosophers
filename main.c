be0fabbad355f0a8dd9c2336df781e50e005cde5
eweiberl <eweiberl@student.42heilbronn.de>
2023-09-14 14:54:35 +0200

    added leaks check

--- main.c
+++ main.c
@@ -6,25 +6,31 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 14:13:45 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/09/08 14:28:29 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/09/14 14:52:42 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+// void	check_leaks(void)
+// {
+// 	system("leaks philo");
+// }
 
 int	main(int argc, char *argv[])
 {
 	t_philo_var		philo_var;
 	t_philosopher	*philo_list;
 
+	// atexit(check_leaks);
 	philo_var.num_has_to_eat = -2;
 	if (get_input(argc, argv, &philo_var) == 1)
 		return (0);
-	printf("Num of philos: %d\n", philo_var.num_of_philo);
-	printf("Time to die: %d\n", philo_var.time_to_die);
-	printf("Time to eat: %d\n", philo_var.time_to_eat);
-	printf("Time to sleep: %d\n", philo_var.time_to_sleep);
-	printf("Num has to eat: %d\n", philo_var.num_has_to_eat);
+	// printf("Num of philos: %d\n", philo_var.num_of_philo);
+	// printf("Time to die: %d\n", philo_var.time_to_die);
+	// printf("Time to eat: %d\n", philo_var.time_to_eat);
+	// printf("Time to sleep: %d\n", philo_var.time_to_sleep);
+	// printf("Num has to eat: %d\n", philo_var.num_has_to_eat);
 	philo_list = init_philo(philo_var);
 	if (philo_list == NULL)
 		return (1);
