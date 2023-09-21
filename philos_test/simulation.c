c84d64b4bc6364fe3b1851693a38024e0aac875a
eweiberl <eweiberl@student.42heilbronn.de>
2023-09-21 18:24:47 +0200

    recoded the monitor threads function

--- philos_test/simulation.c
+++ philos_test/simulation.c
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 17:47:11 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/09/21 18:20:30 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/09/21 18:24:30 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,30 @@ int	simulation(t_philosopher *philo_list)
 	pthread_mutex_unlock(&print_lock);
 }
 
+/// @brief Checks if a philo has dies or all have eaten enough
+/// @param philo_list 
 static void	monitor_threads(t_philosopher *philo_list)
 {
 	t_philosopher	*current;
 
 	current = philo_list;
 	wait_ms(current->philo_var.time_to_die - 15);
+	while (1)
+	{
+		starving(current);
+		pthread_mutex_lock(&current->var_lock);
+		if (current->isdead == true)
+		{
+			pthread_mutex_unlock(&current->var_lock);
+			break ;
+		}
+		pthread_mutex_unlock(&current->var_lock);
+		if (has_eaten_enough(philo_list) == true)
+			break ;
+		current = current->next;
+		if (current == NULL)
+			current = philo_list;
+	}
 }
 
 /// @brief IStarts the threads
