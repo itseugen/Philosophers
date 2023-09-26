e66ad40ff9b5b83c4bad73c400ea26c711bcb947
eweiberl <eweiberl@student.42heilbronn.de>
2023-09-26 15:40:49 +0200

    readded var_lock_name

--- philo_bonus/philo_bonus.h
+++ philo_bonus/philo_bonus.h
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 13:52:12 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/09/26 15:32:30 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/09/26 15:40:34 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,7 @@ typedef struct s_philosopher
 	long					last_meal;
 	sem_t					*print_lock;
 	sem_t					var_lock;
+	char					*var_lock_name;
 	sem_t					*fork_lock;
 }	t_philosopher;
 
