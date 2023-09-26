cf6191d604e4fc4dc9c52ced09bbb8933a5cfb60
eweiberl <eweiberl@student.42heilbronn.de>
2023-09-26 15:55:28 +0200

    changed mutexes into sems

--- philo_bonus/philos/exec_philo.c
+++ philo_bonus/philos/exec_philo.c
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 17:47:03 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/09/26 14:35:10 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/09/26 15:51:13 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,26 @@ void	*philosopher(void *param)
 	t_philosopher	*philo;
 
 	philo = (t_philosopher *)param;
-	pthread_mutex_lock(philo->print_lock);
-	pthread_mutex_unlock(philo->print_lock);
-	pthread_mutex_lock(&philo->var_lock);
+	sem_wait(philo->print_lock);
+	sem_post(philo->print_lock);
+	sem_wait(&philo->var_lock);
 	philo->start_time = *(philo->main_start);
 	philo->last_meal = get_ms(philo->start_time);
 	philo->num_eaten = 0;
-	pthread_mutex_unlock(&philo->var_lock);
+	sem_post(&philo->var_lock);
 	do_first_action(philo);
-	pthread_mutex_lock(&philo->var_lock);
+	sem_wait(&philo->var_lock);
 	while (philo->isdead == false)
 	{
-		pthread_mutex_unlock(&philo->var_lock);
+		sem_post(&philo->var_lock);
 		eat(philo);
 		print_action(philo, SLEEP);
 		wait_ms(philo->philo_var.time_to_sleep);
 		print_action(philo, THINK);
-		pthread_mutex_lock(&philo->var_lock);
+		sem_wait(&philo->var_lock);
 	}
-	pthread_mutex_unlock(&philo->var_lock);
-	return (0);
+	sem_post(&philo->var_lock);
+	exit(0);
 }
 
 static void	do_first_action(t_philosopher *philo)
--- philo_bonus/philos/philo_utils.c
+++ philo_bonus/philos/philo_utils.c
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 18:08:07 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/09/26 14:35:19 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/09/26 15:55:13 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,19 @@ void	starving(t_philosopher *philo)
 
 	if (philo == NULL)
 		return ;
-	pthread_mutex_lock(&philo->var_lock);
+	sem_wait(&philo->var_lock);
 	time_since_last_meal = get_ms(philo->start_time) - philo->last_meal;
 	if (time_since_last_meal >= philo->philo_var.time_to_die)
 	{
-		pthread_mutex_unlock(&philo->var_lock);
+		sem_post(&philo->var_lock);
 		pthread_mutex_lock(philo->print_lock);
 		printf("%ld %d died\n", get_ms(philo->start_time), philo->id);
-		pthread_mutex_lock(&philo->var_lock);
+		sem_wait(&philo->var_lock);
 		philo->isdead = true;
-		pthread_mutex_unlock(&philo->var_lock);
+		sem_post(&philo->var_lock);
 	}
 	else
-		pthread_mutex_unlock(&philo->var_lock);
+		sem_post(&philo->var_lock);
 }
 
 /// @brief makes the philo eat
@@ -58,10 +58,10 @@ void	eat(t_philosopher *philo)
 		if (even_fork_lock(philo) == 1)
 			return ;
 	print_action(philo, EAT);
-	pthread_mutex_lock(&philo->var_lock);
+	sem_wait(&philo->var_lock);
 	philo->last_meal = get_ms(philo->start_time);
 	philo->num_eaten++;
-	pthread_mutex_unlock(&philo->var_lock);
+	sem_post(&philo->var_lock);
 	wait_ms(philo->philo_var.time_to_eat);
 	pthread_mutex_unlock(&philo->fork_lock);
 	pthread_mutex_unlock(&philo->fork_left->fork_lock);
@@ -85,8 +85,8 @@ static int	even_fork_lock(t_philosopher *philo)
 
 void	print_action(t_philosopher *philo, int content)
 {
-	pthread_mutex_lock(philo->print_lock);
-	pthread_mutex_lock(&philo->var_lock);
+	sem_wait(philo->print_lock);
+	sem_wait(&philo->var_lock);
 	if (content == FORK && philo->isdead != true)
 		printf("%ld %d has taken a fork\n",
 			get_ms(philo->start_time), philo->id);
@@ -96,8 +96,8 @@ void	print_action(t_philosopher *philo, int content)
 		printf("%ld %d is sleeping\n", get_ms(philo->start_time), philo->id);
 	else if (content == THINK && philo->isdead != true)
 		printf("%ld %d is thinking\n", get_ms(philo->start_time), philo->id);
-	pthread_mutex_unlock(&philo->var_lock);
-	pthread_mutex_unlock(philo->print_lock);
+	sem_post(&philo->var_lock);
+	sem_post(philo->print_lock);
 }
 
 bool	has_eaten_enough(t_philosopher *philo)
@@ -109,13 +109,13 @@ bool	has_eaten_enough(t_philosopher *philo)
 		return (false);
 	while (current != NULL)
 	{
-		pthread_mutex_lock(&current->var_lock);
+		sem_wait(&current->var_lock);
 		if (current->num_eaten < current->philo_var.num_has_to_eat)
 		{
-			pthread_mutex_unlock(&current->var_lock);
+			sem_post(&current->var_lock);
 			return (false);
 		}
-		pthread_mutex_unlock(&current->var_lock);
+		sem_post(&current->var_lock);
 		current = current->next;
 	}
 	return (true);
